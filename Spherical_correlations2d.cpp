251254fa37fbcd0818f8af499ae974113b232476
Adélie Gorce <33687077+adeliegorce@users.noreply.github.com>
2019-01-17 11:26:39 +0000

    Update Spherical_correlations2d.cpp

--- Spherical_correlations2d.cpp
+++ Spherical_correlations2d.cpp
@@ -4,10 +4,9 @@
 //
 
 /*
- Executable file to compute the spherical correlation function
- as defined in Obreschkow et al., 2013, ApJ, 762:115
- from files with real and imaginary parts of Fourier tranform
- of matter overdensity in 2D box
+ C++ file to compute the spherical correlation function
+ as defined in 
+ from textfiles with real and imaginary parts of Fourier tranform of 2D field
  */
 
 #include <vector>
@@ -31,9 +30,9 @@ using namespace std;
 #endif
 const double pi=3.141592653589793;
 const double s3=sqrt(3.0);
-const int N=SIZE; //sampling number
+const int N=SIZE; //number of pixels
 const int NDIM=2; //dimension of the box
-const double L=LENGTH; //length of the realspace box
+const double L=LENGTH; //length of the realspace box (Mpc)
 const int nthreads = NTHREADS; //number of threads for parallelisation
 
 static int total=0;
@@ -55,6 +54,7 @@ vector<double> range(double min, double max, size_t N) {
     return range;
 }
 
+/* Computes the array of k_vectors of the box */
 vector<double> k_axis() {
     vector<double> y;
     double delta = 2*pi/L;
@@ -67,8 +67,8 @@ vector<double> k_axis() {
     return y;
 }
 
-static double overdensity_kr[N][N], overdensity_ki[N][N];
-static complex<double> overdensity_k[N][N], epsilon_k[N][N];
+static double field_kr[N][N], field_ki[N][N]; //real and imaginary parts of FT of field (read from files)
+static complex<double> field_k[N][N], epsilon_k[N][N]; //full FT and phase factor of field
 static vector<double> k_x=k_axis();
 const double delta_k = 2.0*pi/L;
 
@@ -78,7 +78,7 @@ complex<double> sigma_plus(int i, int j, int i2, int j2, double r) {
     int sx=0, sy=0;
     double norm_p=0, window;
 
-    if (i==N | j==N | i2==N | j2==N) window=0; //in main, loops start at i=0, but there is no symmetrical term (sigma(N-i)) as it would have index N-i = N which does not exist in overdensity array: need to remove this contribution from final sum by saying it is zero
+    if (i==N | j==N | i2==N | j2==N) window=0; //in main, loops start at i=0, but there is no symmetrical term (sigma(N-i)) as it would have index N-i = N which does not exist in field array: need to remove this contribution from final sum by saying it is zero
     else {
         vector<double> p(NDIM);
         p[0]=k_x[i]+k_x[i2]*0.5+s3*0.5*k_x[j2];
@@ -89,7 +89,7 @@ complex<double> sigma_plus(int i, int j, int i2, int j2, double r) {
         B=epsilon_k[i2][j2]*epsilon_k[i][j]*conj(epsilon_k[sx][sy]);
         // p vector
         norm_p=sqrt(pow(p[0],2)+pow(p[1],2));
-	//window function
+	//2D window function
         window=jn(0,norm_p*r);//Bessel function of the first kind order zero
         if (i==floor(N*0.5) | j==floor(N*0.5) | i2==floor(N*0.5) | j2==floor(N*0.5)) B*=0.5; //N/2th term will be counted 2 times because of symmetries in main function so need to divide value by 2
     }
@@ -125,13 +125,12 @@ complex<double> add_sigma(int i, int j, int i2, int j2, double r) {
 }
 
 complex<double> sum_sigma(double r) {
-// double sum_sigma(double r) {
  
     // Initialisation
-    //int max_loop=min_int(floor(N*0.5),floor(pi/(r*delta_k))); //max value of loop index : k,q <= pi/r condition
     double sigma_real=0,sigma_imag=0;
     complex<double> sigma=(0,0);
     
+    // builds an array of all the possible values of norm(k) we will sum over (including lim k < pi/r)
     vector<int> v(NDIM);
     int u=1;
     double *k_norm, kni=0;
@@ -158,7 +157,8 @@ complex<double> sum_sigma(double r) {
     int ksize=u;
     total+=ksize;
     cout << ksize << " / " <<flush;
-
+	
+    // start of parallelisation
     omp_set_num_threads(nthreads);
     #pragma omp parallel 
     {
@@ -167,11 +167,11 @@ complex<double> sum_sigma(double r) {
     vector<double> k(NDIM), q(NDIM);
 
     #pragma omp for reduction (+: sigma_real)
+    // loop over array of k_norms
     for (int i=0; i<ksize; i++) {
         for (int j=0; j<ksize; j++) {
             if ( k_norm[i]<= pi/r
             && k_norm[j]<= pi/r ) {
-            //&& sqrt(pow(k_x[k_ind[i][0]]+k_x[k_ind[j][0]],2)+pow(k_x[k_ind[i][1]]+k_x[k_ind[j][1]],2) ) <= pi/r) {
                 sigma_real+= real(add_sigma(k_ind[i][0],k_ind[i][1],k_ind[j][0],k_ind[j][1],r));
                 sigma_real+= real(add_sigma(k_ind[i][1],k_ind[i][0],k_ind[j][0],k_ind[j][1],r));
                 sigma_real+= real(add_sigma(k_ind[i][0],k_ind[i][1],k_ind[j][1],k_ind[j][0],r));
@@ -198,27 +198,27 @@ int main() {
     string real_file=string(filename)+string("_realpart.txt");
     string imag_file=string(filename)+string("_imagpart.txt");
 
-    //Reading overdensity files
-    ifstream overdensity_file_r, overdensity_file_i ;
-    overdensity_file_r.open(real_file);
-    overdensity_file_i.open(imag_file);
-    if (!overdensity_file_r.is_open() | !overdensity_file_i.is_open() ) {cerr << "Unable to open file" <<endl; return -1;}
+    //Reading field files
+    ifstream field_file_r, field_file_i ;
+    field_file_r.open(real_file);
+    field_file_i.open(imag_file);
+    if (!field_file_r.is_open() | !field_file_i.is_open() ) {cerr << "Unable to open file" <<endl; return -1;}
     cout << "Overdensity files opened: reading..." <<endl ;
     for (int i=0; i<N; i++) {
         for (int j=0; j<N; j++) {
-            overdensity_file_r >> overdensity_kr[i][j];
-            overdensity_file_i >> overdensity_ki[i][j];
-            overdensity_k[i][j] = complex<double>(overdensity_kr[i][j] , overdensity_ki[i][j]);
-            if (abs(overdensity_k[i][j])<delta_k) epsilon_k[i][j]=0;
-            else epsilon_k[i][j] = overdensity_k[i][j]/abs(overdensity_k[i][j]);
+            field_file_r >> field_kr[i][j];
+            field_file_i >> field_ki[i][j];
+            field_k[i][j] = complex<double>(field_kr[i][j] , field_ki[i][j]);
+            if (abs(field_k[i][j])<delta_k) epsilon_k[i][j]=0;
+            else epsilon_k[i][j] = field_k[i][j]/abs(field_k[i][j]);//phase factor
         }
     }
 
     cout << "Done reading files, starting to loop" <<endl;
-    overdensity_file_r.close();
-    overdensity_file_i.close();
+    field_file_r.close();
+    field_file_i.close();
 
-
+    // Writes results in output textfile
     string outfilename = string(filename)+string("_L")+to_string(int(L))+string("_spherical_correlations.txt");
     ofstream outfile;
     outfile.open(outfilename);
@@ -226,10 +226,9 @@ int main() {
     outfile << "# r Re[s(r)] Im[s(r)]" <<endl;
 
     int nbins=500;
-    vector<double> r=range(0.5,50.,nbins);
+    vector<double> r=range(0.5,50.,nbins); //range of correlations scales for which s(r) is computed
     complex<double> l=(0,0);
-    auto start = chrono::steady_clock::now();
-    //complex<double> l(0,0);]
+    auto start = chrono::steady_clock::now(); //time
     cout << " r / size of k array / s(r)"  <<endl;
     for (int i=0; i<nbins; i++) {
         cout << r[i] << " / " <<flush;
@@ -239,16 +238,18 @@ int main() {
     }
     outfile.close();
 
+    // Prints info about computation
     cout << "Total number of steps: " << total<<endl;
     auto end = chrono::steady_clock::now();
     auto howlong = end - start;
     cout << "Executing time: " << chrono::duration <double> (howlong).count()/60 << "min" <<endl;
 
+    // stores number of iterations in output file
     ofstream arrayfile;
     arrayfile.open("array_sizes.txt",fstream::app);
     arrayfile << "NDIM=" << NDIM << " N=" << N << " L=" << L<< " total ksize = " << total <<endl;
     arrayfile.close();
-
+    // stores computing time in output file
     ofstream timefile;
     timefile.open("executing_times.txt", fstream::app);
     timefile << "NDIM=" << NDIM << " N=" << N << " L=" << L << " threads: " << nthreads << " time = " << chrono::duration <double> (howlong).count()/60 << "min" <<endl;
