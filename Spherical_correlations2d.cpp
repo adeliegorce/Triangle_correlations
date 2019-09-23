4404dcef99101c53cfdb26849e570386e219a7df
adelie gorce <adelie.gorce@ias.u-psud.fr>
2019-09-23 18:00:07 +0200

    New version with FFTW included in programme

--- Spherical_correlations2d.cpp
+++ Spherical_correlations2d.cpp
@@ -4,36 +4,38 @@
 //
 
 /*
- C++ file to compute the spherical correlation function
- as defined in 
- from textfiles with real and imaginary parts of Fourier tranform of 2D field
+ Executable file to compute the spherical correlation function
+ as defined in Obreschkow et al., 2013, ApJ, 762:115
+ from files with real and imaginary parts of Fourier tranform
+ of matter field in 2D box
  */
 
 #include <vector>
 #include <math.h>
-#include <omp.h>
+// #include <omp.h>
 #include <algorithm>
-#include <complex>
 using namespace std;
 #include <iostream>
+#include <complex.h>
 #include <fstream>
 #include <string>
 #include <stdio.h>
 #include <stdlib.h>
 #include <chrono>
+#include <fftw3.h>
 
 #ifndef LENGTH
 #define LENGTH SIZE
 #endif
-#ifndef NTHREADS
-#define NTHREADS 10
-#endif
+// #ifndef NTHREADS
+// #define NTHREADS 10
+// #endif
 const double pi=3.141592653589793;
 const double s3=sqrt(3.0);
-const int N=SIZE; //number of pixels
+const int N=SIZE; //sampling number
 const int NDIM=2; //dimension of the box
-const double L=LENGTH; //length of the realspace box (Mpc)
-const int nthreads = NTHREADS; //number of threads for parallelisation
+const double L=LENGTH; //length of the realspace box
+// const int nthreads = NTHREADS; //number of threads for parallelisation
 
 static int total=0;
 
@@ -54,7 +56,6 @@ vector<double> range(double min, double max, size_t N) {
     return range;
 }
 
-/* Computes the array of k_vectors of the box */
 vector<double> k_axis() {
     vector<double> y;
     double delta = 2*pi/L;
@@ -67,8 +68,7 @@ vector<double> k_axis() {
     return y;
 }
 
-static double field_kr[N][N], field_ki[N][N]; //real and imaginary parts of FT of field (read from files)
-static complex<double> field_k[N][N], epsilon_k[N][N]; //full FT and phase factor of field
+static complex<double> epsilon_k[N][N];
 static vector<double> k_x=k_axis();
 const double delta_k = 2.0*pi/L;
 
@@ -89,7 +89,7 @@ complex<double> sigma_plus(int i, int j, int i2, int j2, double r) {
         B=epsilon_k[i2][j2]*epsilon_k[i][j]*conj(epsilon_k[sx][sy]);
         // p vector
         norm_p=sqrt(pow(p[0],2)+pow(p[1],2));
-	//2D window function
+	//window function
         window=jn(0,norm_p*r);//Bessel function of the first kind order zero
         if (i==floor(N*0.5) | j==floor(N*0.5) | i2==floor(N*0.5) | j2==floor(N*0.5)) B*=0.5; //N/2th term will be counted 2 times because of symmetries in main function so need to divide value by 2
     }
@@ -125,12 +125,12 @@ complex<double> add_sigma(int i, int j, int i2, int j2, double r) {
 }
 
 complex<double> sum_sigma(double r) {
+// double sum_sigma(double r) {
  
     // Initialisation
+    //int max_loop=min_int(floor(N*0.5),floor(pi/(r*delta_k))); //max value of loop index : k,q <= pi/r condition
     double sigma_real=0,sigma_imag=0;
-    complex<double> sigma=(0,0);
     
-    // builds an array of all the possible values of norm(k) we will sum over (including lim k < pi/r)
     vector<int> v(NDIM);
     int u=1;
     double *k_norm, kni=0;
@@ -157,21 +157,20 @@ complex<double> sum_sigma(double r) {
     int ksize=u;
     total+=ksize;
     cout << ksize << " / " <<flush;
-	
-    // start of parallelisation
-    omp_set_num_threads(nthreads);
-    #pragma omp parallel 
-    {
+
+    // omp_set_num_threads(nthreads);
+    // #pragma omp parallel 
+    // {
 
     double norm_k=0, norm_q=0;
     vector<double> k(NDIM), q(NDIM);
 
-    #pragma omp for reduction (+: sigma_real)
-    // loop over array of k_norms
+    // #pragma omp for reduction (+: sigma_real)
     for (int i=0; i<ksize; i++) {
         for (int j=0; j<ksize; j++) {
             if ( k_norm[i]<= pi/r
             && k_norm[j]<= pi/r ) {
+            //&& sqrt(pow(k_x[k_ind[i][0]]+k_x[k_ind[j][0]],2)+pow(k_x[k_ind[i][1]]+k_x[k_ind[j][1]],2) ) <= pi/r) {
                 sigma_real+= real(add_sigma(k_ind[i][0],k_ind[i][1],k_ind[j][0],k_ind[j][1],r));
                 sigma_real+= real(add_sigma(k_ind[i][1],k_ind[i][0],k_ind[j][0],k_ind[j][1],r));
                 sigma_real+= real(add_sigma(k_ind[i][0],k_ind[i][1],k_ind[j][1],k_ind[j][0],r));
@@ -182,9 +181,9 @@ complex<double> sum_sigma(double r) {
     
     sigma_imag=0;
 	    
-    }//end of parallelisation
+    // }//end of parallelisation
 
-    sigma=complex<double>(sigma_real,sigma_imag);
+    complex<double> sigma=complex<double>(sigma_real,sigma_imag);
 
     return sigma*pow(r/L,NDIM*1.5);
 
@@ -192,69 +191,96 @@ complex<double> sum_sigma(double r) {
 
 
 int main() {
-  
-    cout << "Dim in Fourier space " << N << " ; real space length " << L << " ; number of threads " << nthreads <<endl; 
 
-    string real_file=string(filename)+string("_realpart.txt");
-    string imag_file=string(filename)+string("_imagpart.txt");
+    cout << "Dim in Fourier space " << N << " ; real space length " << L << endl;//" ; number of threads " << nthreads <<endl; 
+
+    string file=string(filename_box)+string(".txt");
+
+    // double *field;
+    // field = (double*)fftw_malloc(sizeof(double) * (N) * (N));
+    fftw_complex *field;
+    field = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * (N) * (N));
 
     //Reading field files
-    ifstream field_file_r, field_file_i ;
-    field_file_r.open(real_file);
-    field_file_i.open(imag_file);
-    if (!field_file_r.is_open() | !field_file_i.is_open() ) {cerr << "Unable to open file" <<endl; return -1;}
-    cout << "Overdensity files opened: reading..." <<endl ;
+    ifstream field_file ;
+    field_file.open(file);
+    if (!field_file.is_open() ) {cerr << "Unable to open file" <<endl; return -1;}
+    cout << "Field file opened: reading..." <<endl ;
     for (int i=0; i<N; i++) {
         for (int j=0; j<N; j++) {
-            field_file_r >> field_kr[i][j];
-            field_file_i >> field_ki[i][j];
-            field_k[i][j] = complex<double>(field_kr[i][j] , field_ki[i][j]);
-            if (abs(field_k[i][j])<delta_k) epsilon_k[i][j]=0;
-            else epsilon_k[i][j] = field_k[i][j]/abs(field_k[i][j]);//phase factor
+            field_file >> field[i + N * j][0];
+            field[i + N * j][1]=0;
+            cout << field[i + N * j][0] <<flush;
+        }
+        // cout << " " <<endl;
+    }
+
+    cout << "Done reading file, computing FFT" <<endl;
+    field_file.close();
+
+
+    fftw_complex *field_k;
+    field_k = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N * (N));
+
+    fftw_plan p;
+    p = fftw_plan_dft_2d(N, N, field, field_k, FFTW_FORWARD, FFTW_ESTIMATE);
+    // p = fftw_plan_dft_r2c_2d(N, N, field, field_k, FFTW_ESTIMATE);
+    fftw_execute(p);
+
+    // cout << field_k[0 + N * 0][0] << " " << field_k[0 + N * 0][1] <<endl;
+    // cout << field_k[12 + N * 35][0] << " " << field_k[12 + N * 35][1] <<endl;
+
+    cout << field_k[12 + N * 12][0] << " " << field_k[12 + N * 12][1] <<endl;
+    cout << field_k[(N-12) + N * (N-12)][0] << " " << field_k[(N-12) + N * (N-12)][1] <<endl;
+    double abs=0;
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<N; j++) {
+            abs = sqrt( pow(field_k[i + N * j][0],2) + pow(field_k[i + N * j][1],2) );
+            if ( abs<delta_k) epsilon_k[i][j]=0;
+            else epsilon_k[i][j] = complex<double>(field_k[i + N * j][0],field_k[i + N * j][1])/abs;
         }
     }
 
-    cout << "Done reading files, starting to loop" <<endl;
-    field_file_r.close();
-    field_file_i.close();
+    // cout << epsilon_k[49][49] <<endl;
 
-    // Writes results in output textfile
-    string outfilename = string(filename)+string("_L")+to_string(int(L))+string("_spherical_correlations.txt");
+    string outfilename = string(filename_box)+string("_L")+to_string(int(L))+string("_spherical_correlations.txt");
     ofstream outfile;
     outfile.open(outfilename);
     outfile << "# dim " << N << endl;
     outfile << "# r Re[s(r)] Im[s(r)]" <<endl;
 
-    int nbins=500;
-    vector<double> r=range(0.5,50.,nbins); //range of correlations scales for which s(r) is computed
-    complex<double> l=(0,0);
-    auto start = chrono::steady_clock::now(); //time
+    int nbins=10;
+    vector<double> r=range(0.5,50.,nbins);
+    auto start = chrono::steady_clock::now();
+    //complex<double> l(0,0);]
     cout << " r / size of k array / s(r)"  <<endl;
     for (int i=0; i<nbins; i++) {
         cout << r[i] << " / " <<flush;
-        l=sum_sigma(r[i]);
+        complex<double> l=sum_sigma(r[i]);
         outfile << r[i] << " " << real(l) << " " << imag(l) <<endl;//<< real(l) << " " << imag(l) <<endl;
         cout << l << "     " <<endl;
     }
     outfile.close();
 
-    // Prints info about computation
     cout << "Total number of steps: " << total<<endl;
     auto end = chrono::steady_clock::now();
     auto howlong = end - start;
     cout << "Executing time: " << chrono::duration <double> (howlong).count()/60 << "min" <<endl;
 
-    // stores number of iterations in output file
     ofstream arrayfile;
     arrayfile.open("array_sizes.txt",fstream::app);
     arrayfile << "NDIM=" << NDIM << " N=" << N << " L=" << L<< " total ksize = " << total <<endl;
     arrayfile.close();
-    // stores computing time in output file
-    ofstream timefile;
-    timefile.open("executing_times.txt", fstream::app);
-    timefile << "NDIM=" << NDIM << " N=" << N << " L=" << L << " threads: " << nthreads << " time = " << chrono::duration <double> (howlong).count()/60 << "min" <<endl;
-    timefile.close();
 
+    // ofstream timefile;
+    // timefile.open("executing_times.txt", fstream::app);
+    // timefile << "NDIM=" << NDIM << " N=" << N << " L=" << L << " threads: " << nthreads << " time = " << chrono::duration <double> (howlong).count()/60 << "min" <<endl;
+    // timefile.close();
     
+    // fftw_destroy_plan(p);
+    fftw_free(field);
+    fftw_free(field_k);
+
     return 0;
+
 }
